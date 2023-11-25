83d258d3a89564759cd5539d07abe2a0ea3109f5
vivekrudra980 <vivekprudra@gmail.com>
2023-11-25 10:32:55 +0000

    first commit

---
+++ src/components/EmojiCard/index.js
@@ -0,0 +1,21 @@
+// Write your code here.
+import './index.css'
+
+const EmojiCard = props => {
+  const {emojiDetails, clickEmoji} = props
+  const {id, emojiName, emojiUrl} = emojiDetails
+
+  const onClickEmojiCard = () => {
+    clickEmoji(id)
+  }
+
+  return (
+    <li className="emoji-item">
+      <button className="emoji-btn" type="button" onClick={onClickEmojiCard}>
+        <img src={emojiUrl} alt={emojiName} />
+      </button>
+    </li>
+  )
+}
+
+export default EmojiCard
